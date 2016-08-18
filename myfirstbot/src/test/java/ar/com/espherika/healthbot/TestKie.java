1ea99f07607279e98f38b09f1cb6f7c49eb553e0
carbonecar <carbonecar@gmail.com>
2016-08-17 23:40:10 -0300

    agregado de reglas simples para testear la dependencia del kie

---
+++ myfirstbot/src/test/java/ar/com/espherika/healthbot/TestKie.java
@@ -0,0 +1,26 @@
+package ar.com.espherika.healthbot;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+
+public class TestKie {
+
+	
+	/**
+	 * Con este test validamos que la configuracion de drools este correctamente seteada
+	 */
+	@Test
+	public void testClassLoaderKie() {
+		KieServices kieSerivces = KieServices.Factory.get();
+		// Leemos del classpath las reglas que aqui hay.
+		KieContainer kieContainer = kieSerivces.getKieClasspathContainer();
+
+		KieSession kieSession = kieContainer.newKieSession("ksession");
+		int rulesFired=kieSession.fireAllRules();
+		Assert.assertEquals(2, rulesFired);
+	}
+	
+}
