7defebb94607b463a0ca4000cb16cfffcd39d473
carbonecar <carbonecar@gmail.com>
2016-08-18 00:26:05 -0300

    agregado de reglas para validar el kie stateful

---
+++ myfirstbot/src/main/java/ar/com/espherika/healthbot/model/Ciudadano.java
@@ -0,0 +1,33 @@
+package ar.com.espherika.healthbot.model;
+
+public class Ciudadano {
+
+	private int edad;
+	private String nombre;
+
+	
+	public Ciudadano(){
+		
+	}
+	
+	public Ciudadano(int edad,String nombre){
+		this.edad=edad;
+		this.nombre=nombre;
+	}
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+}
--- myfirstbot/src/test/java/ar/com/espherika/healthbot/TestKie.java
+++ myfirstbot/src/test/java/ar/com/espherika/healthbot/TestKie.java
@@ -6,6 +6,8 @@ import org.kie.api.KieServices;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
 
+import ar.com.espherika.healthbot.model.Ciudadano;
+
 public class TestKie {
 
 	
@@ -21,6 +23,8 @@ public class TestKie {
 		KieSession kieSession = kieContainer.newKieSession("ksession");
 		int rulesFired=kieSession.fireAllRules();
 		Assert.assertEquals(2, rulesFired);
+		kieSession.insert(new Ciudadano(17,"carlos"));
+		Assert.assertEquals(1,kieSession.fireAllRules());
 	}
 	
 }
