84437a7f3a0b48cd9de4bbb220c0157ef8da9502
carbonecar <carbonecar@gmail.com>
2016-09-14 15:49:50 -0300

    fix del bug donde no adopta el habito beber agua

--- myfirstbot/src/main/java/ar/com/espherika/MyFirstBot.java
+++ myfirstbot/src/main/java/ar/com/espherika/MyFirstBot.java
@@ -165,6 +165,8 @@ public class MyFirstBot extends TelegramLongPollingBot {
 				"Fix de las correcciones ortograficas");
 		this.sendControlledMessage(sendMessage,
 				"Fix del issue #2 ");
+		this.sendControlledMessage(sendMessage,
+				"Fix del problema que no adopta el habito de beber agua");
 
 	}
 
